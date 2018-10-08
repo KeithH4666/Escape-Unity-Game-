8c7df3a8fe981f46b70f273be410077b3c4ec09f
Keith Higgins <keithhigginsowns@gmail.com>
2018-10-08 16:55:58 +0100

    Added simple splashscreen animation

---
+++ Assets/Scenes/Fade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour {
+
+    public Image splashImage;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f); // sets the transparcy to invisable
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f,1.5f,false);
+    }
+
+    void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+    
+
+}
