def48e6b079efc291aa5d12a9f0ebc5ecaef482b
Keith Higgins <keithhigginsowns@gmail.com>
2018-11-22 01:25:25 +0000

    added connection to db

---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour {
+
+    public InputField namefield;
+    public InputField pass;
+
+    public Button submit;
+
+    public void callRegister()
+    {
+        Debug.Log(namefield.text + " " + pass.text);
+        StartCoroutine(RegisterUser());
+    }
+
+    IEnumerator RegisterUser()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("name", namefield.text);
+        form.AddField("pass", pass.text);
+
+        WWW www = new WWW("https://leaderboardgmit.000webhostapp.com/sqlconnect.php", form);
+        yield return www;
+
+        if(www.text == "0")
+        {
+            Debug.Log("user created");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+        }
+        else
+        {
+            Debug.Log("Error " + www.text);
+        }
+
+    }
+
+    public void verifyInputs()
+    {
+        submit.interactable = (namefield.text.Length > 8 && pass.text.Length > 8);
+    }
+
+}
